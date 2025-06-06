2e353eeae3dc2c5185f57b2fb52cc02740e09a5d
Noor Piash <noorpiash1234@gmail.com>
2025-06-06 18:26:42 +0600

    my application

--- index.js
+++ index.js
@@ -48,14 +48,28 @@ async function run() {
 
     app.get("/job-application", async (req, res) => {
       const email = req.query.email;
-      const query = { applicant_email: email };
+      const query = { application_email: email };
       const result = await jobApplicationCollection.find(query).toArray();
+
+      for (const application of result) {
+        console.log(application.job_id);
+        const query1 = { _id: new ObjectId(application.job_id) };
+        const job = await jobsCollection.findOne(query1);
+        if (job) {
+          application.title = job.title;
+          application.company = job.company;
+          application.company_logo = job.company_logo;
+          application.location = job.location;
+        }
+      }
+
       res.send(result);
     });
 
     app.post("/job-application", async (req, res) => {
       const application = req.body;
       const result = await jobApplicationCollection.insertOne(application);
+
       res.send(result);
     });
   } finally {
